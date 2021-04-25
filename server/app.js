c4a1d23e29db567aae13d833911856ef7050f8f6
ttuom1nen <ttuominen@outlook.com>
2021-04-25 21:44:26 +0300

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+  })
+);
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log("Listening on port ", PORT);
+});
