3c8e970d553bc881109b3c01377f69f85a962953
ttuom1nen <ttuominen@outlook.com>
2021-04-27 22:51:59 +0300

    Add hobby type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,21 @@
 const graphql = require("graphql");
+const _ = require("lodash");
+
+// Dummy data
+const usersData = [
+  { id: "1", name: "Bond", age: 33, profession: "" },
+  { id: "2", name: "Nannie", age: 18, profession: "" },
+  { id: "3", name: "Nicholas", age: 65, profession: "" },
+  { id: "4", name: "Elsie", age: 94, profession: "" },
+  { id: "5", name: "Kyle", age: 28, profession: "" },
+];
+
+const hobbiesData = [
+  { id: "233", title: "Lebanon", description: "" },
+  { id: "35", title: "Tajikistan", description: "" },
+  { id: "57", title: "Niue", description: "" },
+  { id: "34", title: "South Sudan", description: "" },
+];
 
 const {
   GraphQLObjectType,
@@ -11,11 +28,22 @@ const {
 // Create types
 const UserType = new GraphQLObjectType({
   name: "User",
-  description: "Documentation for user...",
+  description: "User desc",
   fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    profession: { type: GraphQLString },
+  }),
+});
+
+const HobbyType = new GraphQLObjectType({
+  name: "Hoddy",
+  description: "Hobby desc",
+  fields: () => ({
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    description: { type: GraphQLString },
   }),
 });
 
@@ -28,11 +56,21 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
 
-      resolve(parent, args) {},
+      resolve(parent, args) {
+        return _.find(usersData, { id: args.id });
+      },
+    },
+    hobby: {
+      type: HobbyType,
+      args: { id: { type: GraphQLID } },
+
+      resolve(parent, args) {
+        return _.find(hobbiesData, { id: args.id });
+      },
     },
   },
 });
 
-module.export = new GraphQLSchema({
+module.exports = new GraphQLSchema({
   query: RootQuery,
 });
