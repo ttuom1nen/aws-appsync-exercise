49d1b0c182532cbd7d2a6832a5911f65ab4f7d57
ttuom1nen <ttuominen@outlook.com>
2021-04-29 22:59:42 +0300

    Add create post mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -167,6 +167,24 @@ const Mutation = new GraphQLObjectType({
         return user;
       },
     },
+
+    createPost: {
+      type: PostType,
+      args: {
+        //id: { type: GraphQLID },
+        comment: { type: GraphQLString },
+        userId: { type: GraphQLID },
+      },
+
+      resolve(parent, args) {
+        let post = {
+          comment: args.comment,
+          userId: args.userId,
+        };
+
+        return post;
+      },
+    },
   },
 });
 
