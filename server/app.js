f50effa81c7743fd0968abaa224d17ceb8184c4e
ttuom1nen <ttuominen@outlook.com>
2021-05-05 21:46:52 +0300

    Use env file for mongodb

--- server/app.js
+++ server/app.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
+// const mongodb = process.env.MONGODB_URL;
+// console.log(mongodb);
 
 const schema = require("./schema/schema");
-const testSchema = require("./schema/types_schema");
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("Connected!");
+});
 
 app.use(
   "/graphql",
   graphqlHTTP({
     graphiql: true,
-    schema: testSchema,
+    schema: schema,
   })
 );
 
-const PORT = 4000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log("Listening on port ", PORT);
